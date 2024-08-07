26688d7ea072c29483b4792d04bb90b116d7440e
pabloozamora <pabloozamora02@gmail.com>
2024-08-06 23:22:01 -0600

    feat: XMPPClient initial config

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import XMPPClient from './components/XmppChat/XMPPClient'
+
+function App() {
+
+  return (
+    <div className="App">
+      <XMPPClient />
+    </div>
+  )
+}
+
+export default App
